506c6b8e11b47ff0fb94b46cedd1c6f2618e6c03
erykestabillo <erykestabillo@gmail.com>
2019-11-20 13:10:13 +0800

    Removed files that are in .gitignore

--- drfvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/cjdango/Desktop/drf/trello_drf/drfvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- trello_drf_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TrelloDrfAppConfig(AppConfig):
-    name = 'trello_drf_app'
